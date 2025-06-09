a30c299dbeb2e1424efcdb3367d1b8be44753978
Kagaya2004 <kaykyogaya@gmail.com>
2025-06-08 22:16:14 -0300

    Forms do User

---
+++ GamesGateServer/resources/js/app/src/views/user/UserFormList.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react'
+import axiosClient from '../../axiosClient';
+import { data, Link } from 'react-router-dom';
+
+function UserFormList() {
+
+  const [users, setUsers] = React.useState([]);
+  //const {page, pageSize} = userParams();
+
+  const getUsers = () => {
+    axiosClient.get(`/user/index?page{}`)
+              .then(({data}) => {
+                setUsers(data.data);
+              }
+              ).catch((error) => {
+                console.log(error);
+              })
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+
+  return (
+    <div className='display'>
+      <div className='card animated fadeInDown'>
+        
+        <div style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center', 
+        }}>
+
+          <h1>Lista de usuários</h1>
+          <Link to="/user/store" className='btn-add'>Store</Link>
+        </div>
+        <table>
+          
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Apelido</th>
+              <th>Email</th>
+              <th>Data de Nascimento</th>
+              <th>Descrição</th>
+              <th>Status</th>
+              <th className='center actions' colSpan={3}>Ações</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              users.length > 0 ? (
+                users.map((user, index) => (
+                  <tr key={index}>
+
+                    <td>{user.id}</td>
+
+                    <td>{user.nome}</td>
+
+                    <td>{user.apelido}</td>
+
+                    <td>{user.email}</td>
+
+                    <td>{user.dataLancamento}</td>
+
+                    <td>{user.descricao}</td>
+
+                    <td>{user.status}</td>
+
+                    <td className='center actions'>
+                      <Link to={`/user/update/${user.id}`} className='btn-edit'>Update</Link>
+                    </td>
+
+                    <td className='center actions'>
+                      <Link to={`/user/destroy/${user.id}`} className='btn-delete'>Destroy</Link>
+                    </td>
+                  
+                    <td className='center actions'>
+                      <Link to={`/user/show/${user.id}`} className='btn-show'>Show</Link>
+                    </td>
+
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td>Nenhum Registro encontrado</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default UserFormList
---
+++ GamesGateServer/resources/js/app/src/views/user/UserFormStore.jsx
@@ -0,0 +1,133 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import axiosClient from '../../axiosClient';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+function UserFormStore() {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        id: null,
+        nome: '',
+        apelido: '',
+        dataLancamento: new Date().toISOString().split('T')[0],
+        email: '',
+        password: '',
+        descricao: '',
+        status: '',
+    });
+
+    // Função do tipo Anônima
+    const onSubmit = (e) => {
+        e.preventDefault();
+        axiosClient.post(`/user/store`, user)
+            .then(() => {
+                setUser({});
+                console.log('Usuário incluído com sucesso');
+                navigate('/user/index')
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
+    const onCancel = (e) => {
+        navigate('/user/index');
+    }
+
+
+
+    return (
+        <Fragment>
+            <div className="display">
+                <div className="card animated fadeinDown">
+                    <h1>Inclusão de Usuário</h1>
+
+                    <form onSubmit={(e) => onSubmit(e)}>
+                        <input
+                            type="text"
+                            value={user.nome}
+                            placeholder="Nome Completo"
+                            onChange={
+                                e => setUser({
+                                    ...user, name: e.target.value
+                                })
+                            }
+                        />
+                        <input
+                            type="text"
+                            value={user.apelido}
+                            placeholder="Apelido do Usuário"
+                            onChange={
+                                e => setUser({
+                                    ...user, apelido: e.target.value
+                                })
+                            }
+                        />
+                        <input
+                            type='date'
+                            value={user.dataNascimento}
+                            placeholder="Data de Nascimento"
+                            onChange={
+                                e => setUser({
+                                    ...user, dataNascimento: e.target.value
+                                })
+                            }
+                        />
+                        <input
+                            value={user.email}
+                            placeholder="Email"
+                            onChange={
+                                e => setUser({
+                                    ...user, email: e.target.value
+                                })
+                            }
+                        />
+                        <input
+                            type="password"
+                            value={user.senha}
+                            placeholder="Senha"
+                            onChange={
+                                e => setUser({
+                                    ...user, senha: e.target.value
+                                })
+                            }
+                        />
+                        <input
+                            type="text"
+                            value={user.descricao}
+                            placeholder="Descrição"
+                            onChange={
+                                e => setUser({
+                                    ...user, descricao: e.target.value
+                                })
+                            }
+                        />
+                        <input
+                            type="text"
+                            value={user.status}
+                            placeholder="Status"
+                            onChange={
+                                e => setUser({
+                                    ...user, Status: e.target.value
+                                })
+                            }
+                        />
+                        <br />
+                        <br />
+                        <button
+                            className="btn btn-edit">
+                            Salvar
+                        </button>
+                        <Link
+                            type='button'
+                            className='btn btn-cancel'
+                            to='/user/index'>
+                            Cancelar
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default UserFormStore
